31ee0f93db5a9ecf0a356f9045bfe2a14b589a2c
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-03 10:07:30 +0300

    problem 2 alert

---
+++ cpp/ps0003/ps0002_soln.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	char first_letter = 'A';
+	char second_letter = 'N';
+	char third_letter = 'T';
+	char fourth_letter = 'S';
+	char fifth_letter = 'L';
+	double sixth_letter = 'B';
+	float seventh_letter = '7';
+	int eighth_letter = 'F';
+	double tenth_letter = 'H';
+}
