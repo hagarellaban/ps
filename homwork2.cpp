dc6605363e18ee9d91223b3b0da5550f4ec71695
hagarellaban <hellaban564@gmail.com>
2024-07-31 19:07:04 +0300

    problem2

---
+++ homwork2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>  
+int main()
+{
+	short Number_of_people = 100;
+	short Number_of_medicine = 3;
+	short Number_of_team = 10;
+	short Price_of_one_cocies = 20;
+	char First_letter = 'H';
+	double Number_of_baby = 23874673562;
+	double seven_Division_3 = 2.3333333;
+	int electrical_list = 140826;
+	double Age_of_people = 20000000000;
+
+
+	
+
+
+
+}
