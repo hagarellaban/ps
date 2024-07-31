1c3a21aa0e9c9f8ea08b40e6464573d45c416324
hagarellaban <hellaban564@gmail.com>
2024-07-31 19:02:22 +0300

    problem0

---
+++ ps0000.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//we should write # not @ and the library should be <iostream> not <"iostream.h">
+
+int main()
+//donot write the word voit
+{
+    std::cout << "Hello, World!\n";
+    // you should write std:: before cout  and there is erorr in syntax such as ,we should write << not >> and \n not /n
+   /*In C++, we use Cout << “ Text To be Displayed on The screen “;
+     To use Cout << , we must use
+         #include <iostream>;
+     using namespace std;*/
+    return 0;
+    // we write 0 not 1
+}
